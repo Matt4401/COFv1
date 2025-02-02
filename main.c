281b95ed253df906b3c905d9866492361fd6f0e1
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2025-02-02 17:14:14 +0100

    UPDT : add a mini_parsing of the arguments

--- main.c
+++ main.c
@@ -20,11 +20,28 @@ static int set_up_data(void)
     return 0;
 }
 
+static int check_arg(int argc, char *argv[])
+{
+    if (argc != 2) {
+        fprintf(stderr, "\033[1;31mError: Invalid nb of arguments\033[0m\n");
+        return 84;
+    }
+    if (strcmp(argv[1], "encrypt") != 0 && strcmp(argv[1], "decrypt") != 0) {
+        fprintf(stderr, "\033[1;31mError: Invalid arguments\033[0m\n");
+        fprintf(stderr, "\033[31m./COFv1 [encrypt][decrypt]\033[0m\n");
+        return 84;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
+    if (check_arg(argc, argv) == 84)
+        return 84;
     if (get_password() == 84)
         return 84;
-    if (set_up_data() == 84)
-        return 84;
+    if (strcmp(argv[1], "encrypt") == 0)
+        if (set_up_data() == 84)
+            return 84;
     return 0;
 }
